f154dfaed1744454320f9c62c52c0b2c820988dd
ZMY916 <1907027159@qq.com>
2023-04-25 18:11:32 +0800

    test project

---
+++ src/components/NiHao.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>{{ props }}</h2>
+  </div>
+</template>
+
+<script setup lang='ts'>
+    // defineProps()
+    const props =  defineProps(['name'])
+
+</script>
+
+<style  scoped>
+
+</style>
